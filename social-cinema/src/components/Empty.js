f7a2fd62b0e417c7b3d96b6a2dca39013219bb57
emi-hi <emily.hillier@gmail.com>
2019-11-24 02:41:53 +0000

    added components for listing favorite and later movies, hardcoded data, added navbar, added toggling function for showing/hiding lists

---
+++ social-cinema/src/components/Empty.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//this one shows up when there is no favorite. It's just an add symbol.
+export default function Empty(props) {
+  return (
+    <main className="empty">
+      <h1>Empty stuff</h1>
+    </main>
+  );
+}
