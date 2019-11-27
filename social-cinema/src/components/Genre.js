4271f80b262e4a60d19635acb4bfe50f6541ec57
Emily <emily.hillier@gmail.com>
2019-11-26 20:38:37 -0800

    added icons to genre preferences dropdown

--- social-cinema/src/components/Genre.js
+++ social-cinema/src/components/Genre.js
@@ -4,6 +4,7 @@ export default function Genre(props) {
 
   return (
     <div className="genre">
+      <img src={`images/genreicons/${props.name}.svg`} height='50px'/>
       <h4>{props.name}</h4>
       <button type="button" className={props.love_class} onClick={() => props.setGenre(props.id, true)}>Love it!</button>
       <button type="button" className={props.meh_class} onClick={() => props.setGenre(props.id, "")}>Meh</button>
