f4fff508e3f98fc5ead85c5e37d1c94a231a3626
Emily <emily.hillier@gmail.com>
2019-11-27 17:04:54 -0800

    started styling genres, changed font, added hover to change pref

--- social-cinema/src/components/Genre.js
+++ social-cinema/src/components/Genre.js
@@ -1,14 +1,18 @@
 import React from "react";
-
+import '../styles/genre.css';
 export default function Genre(props) {
 
   return (
+    
     <div className="genre">
-      <img src={`images/genreicons/${props.name}.svg`} height='50px' alt={`${props.name}`} />
-      <h4>{props.name}</h4>
-      <button type="button" className={props.hate_class} onClick={() => props.setGenre(props.id, false)}>Hate it!</button>
-      <button type="button" className={props.meh_class} onClick={() => props.setGenre(props.id, "")}>Meh</button>
-      <button type="button" className={props.love_class} onClick={() => props.setGenre(props.id, true)}>Love it!</button>
+      
+      <img src={`images/genreicons/${props.name}.svg`} className="genre-image" alt={`${props.name}`} />
+      <p className="genre-name">{props.name}</p>
+      <div className="genre-choices">
+        <button type="button" id="hategenre" className={props.hate_class} onClick={() => props.setGenre(props.id, false)}>Hate it!</button>
+        <button type="button" id="mehgenre" className={props.meh_class} onClick={() => props.setGenre(props.id, "")}>Meh</button>
+        <button type="button" id="lovegenre" className={props.love_class} onClick={() => props.setGenre(props.id, true)}>Love it!</button>
+      </div>
     </div>
   )
 }
--- social-cinema/src/components/Genres.js
+++ social-cinema/src/components/Genres.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
+import '../styles/genre.css';
 
 import Genre from "./Genre";
 import axios from "axios";
