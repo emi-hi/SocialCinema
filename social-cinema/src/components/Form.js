12cedc8abde191e4fedfbc7278a314796362e2f5
caitlining <caitlincing@gmail.com>
2019-12-01 16:18:55 -0800

    Update colouring and style nav bar

--- social-cinema/src/components/Form.js
+++ social-cinema/src/components/Form.js
@@ -10,17 +10,23 @@ const contentStyle = {
   borderRadius: "20px"
 };
 
+const specialStyle={
+  backgroundColor:"transparent",
+  color: "white",
+  border:0
+}
+
 export default function Form(props) {
   return (
-    <main className="">
+    <>
       <Popup trigger=
         {
-          props.createUser ? <button className="list_name">register</button> : <button className="list_name">sign in</button>
+          props.createUser ? <button style={specialStyle}>Register</button> : <button style={specialStyle}>Sign In</button>
         } modal
         contentStyle={contentStyle}>
         {props.createUser && <Signup createUser={props.createUser} />}
         {props.onLogin && <Login onLogin={props.onLogin} />}
       </Popup>
-    </main>
+    </>
   );
 }
