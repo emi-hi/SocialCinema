18b97f2566d287e922151d8caa9e7715438d83b4
emi-hi <emily.hillier@gmail.com>
2019-12-03 20:11:11 +0000

    added new classname for horizontal friends so it fits the icon size better, changed some logic in the friendlist and style files

--- social-cinema/src/components/FriendList.js
+++ social-cinema/src/components/FriendList.js
@@ -1,6 +1,9 @@
 import React from "react";
 import UserDisplay from "./UserDisplay"
 import './lists/styles.scss'
+let classnames = require("classnames");
+
+
 
 export default function FriendList(props) {
   const friends = props.friends.map((friend) => {
@@ -11,11 +14,19 @@ export default function FriendList(props) {
     return ""
   });
   
+  const friendListHoriz = classnames("list", {
+    "list-friends" : props.action === "add"
+  })
+
+  const friendListColumn = classnames("", {
+    "columnlist" : props.action === "remove"
+  })
+  
   return (
     <article>
       {friends.length>0? <h4 className="computer-title">{props.type}</h4>: ""}
       {props.classname==="list"? <h4 className="mobile-title">{props.type}</h4>: ""}
-      <section className={props.classname}>
+      <section className={props.action === "add"? friendListHoriz: friendListColumn}>
         {friends}
       </section>
     </article>
