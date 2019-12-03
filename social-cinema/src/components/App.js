ab534f8591d360c11d933d9bf27ad2e124ac6cbd
caitlining <caitlincing@gmail.com>
2019-12-03 13:49:17 -0800

    Remove runtime preferences code, add minimum runtime of 20 minutes to all routes

--- social-cinema/src/components/App.js
+++ social-cinema/src/components/App.js
@@ -44,8 +44,8 @@ function App() {
   const [themeList, setThemeList] = useState("hide");
 
   const [recentSuggestions, setRecentSuggestions] = useState([]);
-  const minimumRuntime = 30;
-  const maximumRuntime = 300;
+  const [suggestedMovie, setSuggestedMovie] = useState("");
+  const [suggested, setSuggested] = useState("hide");
 
   const useMovieNight = function(friend, action) {
     if (group.length === 0 && action === "add") {
@@ -192,6 +192,9 @@ function App() {
     setFavList("hide");
     setLaterList("hide");
     setFriendList("hide");
+    setRecentSuggestions([]);
+    setSuggestedMovie("");
+    setSuggested("hide");
   }
 
   return (
@@ -244,8 +247,10 @@ function App() {
           userGenres={userGenres}
           theme={theme}
           setLaterMovies={setLaterMovies}
-          minimumRuntime={minimumRuntime}
-          maximumRuntime={maximumRuntime}
+          suggestedMovie={suggestedMovie}
+          setSuggestedMovie={setSuggestedMovie}
+          suggested={suggested}
+          setSuggested={setSuggested}
         />
         {state.group.length > 0 ?
           <MovieNightFriends  group={group} action="remove" classname="columnlist" useMovieNight={useMovieNight} />:<div></div>
