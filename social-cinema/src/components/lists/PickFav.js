a1958668121ed0ecca1e9569a76ec8d8b87d73df
Emily <emily.hillier@gmail.com>
2019-12-04 15:53:04 -0800

    added alt tag

--- social-cinema/src/components/lists/PickFav.js
+++ social-cinema/src/components/lists/PickFav.js
@@ -6,13 +6,13 @@ export default function PickFav(props) {
   return (
     <>
       <main className="favSelector">
-        <div className="navButton" onClick={() => props.setMovie(prev => prev === 0 ? prev : prev - 1)}><img className="arrow" src="/images/left.svg"/></div>
+        <div className="navButton" onClick={() => props.setMovie(prev => prev === 0 ? prev : prev - 1)}><img alt="left" className="arrow" src="/images/left.svg"/></div>
         <article>
           <img className="favePoster" src={props.searchedMovie[props.movie].poster} alt={props.searchedMovie[props.movie].title} height='200px'></img>
           <h5>{props.searchedMovie[props.movie].title}</h5>
           <h6>{props.searchedMovie[props.movie].release_date}</h6>
         </article>
-        <div className="navButton" onClick={() => props.setMovie(prev => prev === props.searchedMovie.length - 1 ? prev : prev + 1)}><img className="arrow" src="/images/right.svg"/></div>
+        <div className="navButton" onClick={() => props.setMovie(prev => prev === props.searchedMovie.length - 1 ? prev : prev + 1)}><img alt="right" className="arrow" src="/images/right.svg"/></div>
       </main>
       <button onClick={()=>{props.saveToFavoriteList(props.user.name, props.searchedMovie[props.movie]); props.close()}}>Yes, this one!</button>
     </>
